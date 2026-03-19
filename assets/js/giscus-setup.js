e8b16596c9fe35210517bbdcce9deae1a61b0992
gulu-sherry <jiahuishi316@gmail.com>
2026-03-19 09:26:29 +0000

    Deploying to gh-pages from @ gulu-sherry/gulu-sherry.github.io@ce8dbfa5fbdb62bc0bb9f123da36fc69c82bc174 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "gulu-sherry/gulu-sherry.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
